9ea59b034eed142a6c87d6520e97209a8bb0e989
Vladnsk <skul95@mail.ru>
2024-01-11 20:04:16 +0700

    Update ViewController.swift

--- testEdu1/ViewController.swift
+++ testEdu1/ViewController.swift
@@ -20,5 +20,13 @@ final class ViewController: UIViewController {
         // Do any additional setup after loading the view.
 
         // Добавь лэйбл по центру с текстом "тестовый текст"
+        let label = UILabel()
+
+            label.text = "тестовый текст"
+            label.numberOfLines = 0
+        label.frame = CGRectMake(self.view.bounds.size.width/3,self.view.bounds.size.height/2,self.view.bounds.size.width, self.view.bounds.size.height) // x , y, width , height
+        label.textAlignment = .center
+        label.sizeToFit()
+        self.view.addSubview(label)
     }
 }
